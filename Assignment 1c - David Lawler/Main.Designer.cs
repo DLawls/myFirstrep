e8a3644c001d3a1ad1e27cc4aa561a313ccd3f52
david william lawler <davylawler@gmail.com>
2020-10-23 07:13:54 +1100

    Add files via upload

---
+++ Assignment 1c - David Lawler/Main.Designer.cs
@@ -0,0 +1,130 @@
+﻿namespace Assignment_1c___David_Lawler
+{
+    partial class frmMain
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnExit = new System.Windows.Forms.Button();
+            this.btnOpen = new System.Windows.Forms.Button();
+            this.dlgOpen = new System.Windows.Forms.OpenFileDialog();
+            this.btnAverage = new System.Windows.Forms.Button();
+            this.btnTotal = new System.Windows.Forms.Button();
+            this.lblAverage = new System.Windows.Forms.Label();
+            this.lblTotal = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btnExit
+            // 
+            this.btnExit.Location = new System.Drawing.Point(12, 244);
+            this.btnExit.Name = "btnExit";
+            this.btnExit.Size = new System.Drawing.Size(75, 23);
+            this.btnExit.TabIndex = 0;
+            this.btnExit.Text = "Exit";
+            this.btnExit.UseVisualStyleBackColor = true;
+            this.btnExit.Click += new System.EventHandler(this.btnExit_Click);
+            // 
+            // btnOpen
+            // 
+            this.btnOpen.Location = new System.Drawing.Point(12, 12);
+            this.btnOpen.Name = "btnOpen";
+            this.btnOpen.Size = new System.Drawing.Size(159, 52);
+            this.btnOpen.TabIndex = 1;
+            this.btnOpen.Text = "Read File and Fill Array";
+            this.btnOpen.UseVisualStyleBackColor = true;
+            this.btnOpen.Click += new System.EventHandler(this.btnOpen_Click);
+            // 
+            // dlgOpen
+            // 
+            this.dlgOpen.FileName = "openFileDialog1";
+            // 
+            // btnAverage
+            // 
+            this.btnAverage.Location = new System.Drawing.Point(13, 84);
+            this.btnAverage.Name = "btnAverage";
+            this.btnAverage.Size = new System.Drawing.Size(158, 54);
+            this.btnAverage.TabIndex = 2;
+            this.btnAverage.Text = "Average Number of Speeding Fines";
+            this.btnAverage.UseVisualStyleBackColor = true;
+            this.btnAverage.Click += new System.EventHandler(this.btnAverage_Click);
+            // 
+            // btnTotal
+            // 
+            this.btnTotal.Location = new System.Drawing.Point(13, 157);
+            this.btnTotal.Name = "btnTotal";
+            this.btnTotal.Size = new System.Drawing.Size(158, 55);
+            this.btnTotal.TabIndex = 3;
+            this.btnTotal.Text = "Total Number of Speeding Fines";
+            this.btnTotal.UseVisualStyleBackColor = true;
+            this.btnTotal.Click += new System.EventHandler(this.btnTotal_Click);
+            // 
+            // lblAverage
+            // 
+            this.lblAverage.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.lblAverage.Location = new System.Drawing.Point(203, 104);
+            this.lblAverage.Name = "lblAverage";
+            this.lblAverage.Size = new System.Drawing.Size(174, 23);
+            this.lblAverage.TabIndex = 4;
+            // 
+            // lblTotal
+            // 
+            this.lblTotal.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.lblTotal.Location = new System.Drawing.Point(203, 177);
+            this.lblTotal.Name = "lblTotal";
+            this.lblTotal.Size = new System.Drawing.Size(174, 23);
+            this.lblTotal.TabIndex = 5;
+            // 
+            // frmMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(397, 303);
+            this.Controls.Add(this.lblTotal);
+            this.Controls.Add(this.lblAverage);
+            this.Controls.Add(this.btnTotal);
+            this.Controls.Add(this.btnAverage);
+            this.Controls.Add(this.btnOpen);
+            this.Controls.Add(this.btnExit);
+            this.Name = "frmMain";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Assignment 1c";
+            this.Load += new System.EventHandler(this.frmMain_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnExit;
+        private System.Windows.Forms.Button btnOpen;
+        private System.Windows.Forms.OpenFileDialog dlgOpen;
+        private System.Windows.Forms.Button btnAverage;
+        private System.Windows.Forms.Button btnTotal;
+        private System.Windows.Forms.Label lblAverage;
+        private System.Windows.Forms.Label lblTotal;
+    }
+}
+
---
+++ Assignment 1c - David Lawler/Main.cs
@@ -0,0 +1,96 @@
+﻿//*******************************************************************
+// Programmer: David Lawler
+// Date:       24/09/2020
+// Software:   Microsoft Visual Studio 2019 Community Edition
+// Platform:   Microsoft Windows 10 Professional 64­bit Version 1803
+// Purpose:    Reading from a text file to a 2D array - Assignment 1c
+//*******************************************************************
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace Assignment_1c___David_Lawler
+{
+    public partial class frmMain : Form
+    {
+        private StreamReader fileReader;
+        public frmMain()
+        {
+            InitializeComponent();
+        }
+
+        // Form­wide variables
+        string fileName;
+
+        const int ROWS = 4;
+        const int COLUMNS = 2;
+
+        int[,] finesArray = new int[ROWS, COLUMNS];
+
+        private void btnExit_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void btnOpen_Click(object sender, EventArgs e)
+        {
+            string inputRow;
+            string[] inputFines;
+            dlgOpen.Filter = "Text files (*.txt)|*.txt";
+
+            if (dlgOpen.ShowDialog() != DialogResult.Cancel)
+                fileName = dlgOpen.FileName;
+
+            FileStream input = new FileStream(fileName, FileMode.Open, FileAccess.Read);
+
+            fileReader = new StreamReader(input);
+
+            for (int row = 0; row < 4; row++)
+            {
+                inputRow = fileReader.ReadLine(); // Read a record from the file
+                inputFines = inputRow.Split(','); // Store the 2 columns in the string array 
+
+                for (int col = 0; col < 2; col++)
+                {
+                    finesArray[row, col] = Convert.ToInt32(inputFines[col]);
+                }
+                    
+            }
+
+            MessageBox.Show("File was read successfully", "Success!", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            btnOpen.Enabled = false;
+        }
+
+        private void btnAverage_Click(object sender, EventArgs e)
+        {
+            int totalEvening = 0;
+            
+            for (int row = 0; row < 4; row++) //Navigates row 2 only
+                totalEvening += finesArray[row, 1]; //Accumulator
+            lblAverage.Text = "Average Evening Fines = " + Convert.ToString(totalEvening / ROWS);
+            btnAverage.Enabled = false; // Disable button to prevent selection
+        }
+
+        private void btnTotal_Click(object sender, EventArgs e)
+        {
+            int totalAll = 0;
+            for (int row = 0; row < 4; row++)
+                for (int col = 0; col < 2; col++)
+                    totalAll += finesArray[row, col];
+            lblTotal.Text = "Total fines = " + Convert.ToString(totalAll);
+            btnTotal.Enabled = false;
+        }
+
+        private void frmMain_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
